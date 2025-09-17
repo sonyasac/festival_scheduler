4655f55e47096c849c4e792c48426f28958b3671
AG <ag@localhost.localdomain>
2025-09-17 17:22:02 -0500

    add tzinfo

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import streamlit as st
 import pandas as pd
 import datetime
 from streamlit_autorefresh import st_autorefresh
+import pytz
 
 # Run the autorefresh every 2000 milliseconds (2 seconds)
 st_autorefresh(interval=1000, key="data_refresher")
 
 def get_current_time():
-    current_time = datetime.datetime.now()
+    current_time = datetime.datetime.now(pytz.timezone('US/Central'))
     return current_time
 
 def get_theaters(dt_ts):
