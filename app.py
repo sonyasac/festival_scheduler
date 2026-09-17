f385669719ee40d235af7704dc679da6318f36dc
sonyasac <sonya.sachdeva@gmail.com>
2026-09-17 03:12:20 +0000

    update schedule data

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def check_qa_time(block_id):
     qa_time = df[df['block_id'] == block_id]['qa_time_available'].iloc[0]
     return qa_time
 
-df = pd.read_csv('data/csaff25_operations_20250917.csv')
+df = pd.read_csv('data/csaff26_operations_20250916.csv')
 
 current_cols = ['Film Name',
                 'Film Name to Match',
